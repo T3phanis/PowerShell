4d137a303f26015bb50c3fc750ab8a395caf531f
T3phanis <tmcbee24@gmail.com>
2015-01-30 14:59:06 -0500

    website ip address restrictions
    Utilize this script to schedule ip restrictions for a website within
    IIS. Useful for denying access to external ip addresses but keeping the
    site active for internal use / review.

---
+++ site.ip.restrictions.ps1
@@ -0,0 +1,19 @@
+﻿<# 
+.SYNOPSIS 
+Adds IP Restrictions for IIS websites
+
+.DESCRIPTION 
+Begins by shutting down the site, then adds IP address allow / deny rules and then starts the site back up.
+#> 
+
+
+Import-Module WebAdministration
+
+Stop-Website 'SiteName'
+
+#Adds an allow exception#
+Add-WebConfiguration -Filter /system.webserver/security/ipsecurity -Value @{ipAddress="X.X.X.X";subnetMask="X.X.X.X";allowed="true"} -Location "SiteName" -PSPath "IIS:\"
+#Denies everything else#
+Add-WebConfiguration -Filter /system.webserver/security/ipsecurity -Value @{ipAddress="0.0.0.0";subnetmask="255.255.255.255";allowed="false"} -Location "SiteName" -PSPath "IIS:\"
+
+Start-Website 'SiteName'
